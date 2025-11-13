cda69c2f52da302ba7a7121ce5539d8c411fb359
Facundo Zuleta <facundozuleta70@gmail.com>
2025-11-13 20:25:32 -0300

    feat(home): add hero section, colors

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,10 +1,36 @@
+import { GoSearch } from "react-icons/go";
 
 const Hero = () => {
   return (
-    <section>
-        <h1>Hero</h1>
-    </section>
-  )
-}
+    <section className="w-full bg-linear-to-r from-primary-dark/90 to-primary py-20 px-6">
+      <div className="max-w-4xl mx-auto text-center text-white">
 
-export default Hero
+        <h1 className="text-3xl md:text-5xl font-bold leading-tight text-text-primary">
+          Encuentra el freelancer perfecto para tu proyecto
+        </h1>
+
+        <p className="mt-4 text-lg md:text-xl opacity-90 text-text-primary">
+          Miles de profesionales listos para ayudarte con <br />
+          diseño, programación, marketing y más
+        </p>
+
+        <div className="mt-8 flex flex-col md:flex-row items-center justify-center w-full max-w-2xl mx-auto">
+          
+          <div className="flex bg-white p-3 shadow-lg w-full rounded-xl md:rounded-l-xl md:rounded-r-none">
+            <GoSearch className="text-text-secondary-dark mx-2 cursor-pointer" size={24} />
+
+            <input type="text" placeholder="Buscar por servicio o profesional" className="flex-1 outline-none px-2 text-text-secondary-dark" />
+          </div>
+
+          <button className="bg-primary-dark hover:bg-primary-dark/80 text-white font-semibold shadow-lg px-6 py-3 mt-3 md:mt-0 w-full md:w-auto rounded-xl md:rounded-r-xl md:rounded-l-none cursor-pointer">
+            Buscar
+          </button>
+
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,14 @@
 
 import Hero from "../components/home/Hero"
+import Categories from "../components/home/Categories"
+import Services from "../components/home/Services"
 
 const Home = () => {
   return (
     <main>
         <Hero />
+        <Categories />
+        <Services />
     </main>
   )
 }
