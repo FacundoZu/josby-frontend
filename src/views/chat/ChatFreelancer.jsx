444aea03b89b190329f31cfd8341a74fdc9ceedf
Nicolas Nuñez <nuneznicolas582@gmail.com>
2025-11-24 11:26:30 -0300

    Add link to freelancers in footer

--- src/views/chat/ChatFreelancer.jsx
+++ src/views/chat/ChatFreelancer.jsx
@@ -31,13 +31,13 @@ const ChatFreelancer = () => {
   // Auto-scroll
   useEffect(() => {
     if (chatContainerRef.current) {
-      const { scrollHeight, clientHeight } = chatContainerRef.current;
+      const { scrollHeight, clientHeight } = chatContainerRef.current
       
       
       chatContainerRef.current.scrollTo({
         top: scrollHeight - clientHeight,
         behavior: 'smooth'
-      });
+      })
     }
   }, [activeChat?.messages])
 
